e558e2d6713a569be559df478a174f0f9e3e10c0
Matej <matejchalachan@gmail.com>
2021-10-05 20:47:41 +0200

    hexagons updated

--- src/App.vue
+++ src/App.vue
@@ -1,173 +1,172 @@
 <template>
-  <div id="app">
-    <transition-group name="show-hexagons" tag="div" class="hexagons-wrapper">
-      <Hexagon v-for="hexagon in hexagons" :key="hexagon.id" :parentData="hexagon" @selected="selected"/>
-    </transition-group>
-    <div class="score">Score: {{score}}
-    <button class="reset" @click="restartGame" style="float: right">Restart game</button>
+    <div>
+        <div id="app">
+            <transition-group name="show-hexagons" tag="div" class="hexagons-wrapper">
+                <Hexagon v-for="hexagon in hexagons" :key="hexagon.id" :parentData="hexagon" @selected="selected"/>
+            </transition-group>
+        </div>
+
+        <div class="score">Score: {{score}}
+            <button class="reset" @click="restartGame" style="float: right">Restart game</button>
+        </div>
     </div>
-  </div>
+
+
 </template>
 
 <script>
 import Hexagon from './components/Hexagon.vue'
 
 export default {
-  name: 'App',
-  components: {
-    Hexagon
-  },
-  mounted: function() {
-    this.hexagons = this.generateHexagons(20);
-  },
-  data: function() {
-    return {
-      hexagons: [],
-      colors: ['h-color2','h-color1','h-color3'],
-      tempCount: 0,
-    }
-  },
-  computed: {
-    score() {
-      return this.$store.state.score;
+    name: 'App',
+    components: {
+        Hexagon
     },
-    first() {
-      return this.$store.state.first_piece;
+    mounted: function () {
+        this.hexagons = this.generateHexagons(40);
     },
-    second() {
-      return this.$store.state.second_piece;
-    },
-  },
-  methods: {
-    generateHexagons: function (objectCount) {
-      let cards = [];
-      let value = 1;
-      for (let i = 0; i < objectCount; i += 2) {
-
-        let firstObj = {
-          id:i,
-          value: value,
-          showValue: false,
-          color: this.colors[Math.floor(Math.random() * this.colors.length)],
-        };
-        let secondObj = {
-          id: i + 1,
-          value: value,
-          showValue: false,
-          color: this.colors[Math.floor(Math.random() * this.colors.length)],
-        };
-        cards.push(firstObj);
-        cards.push(secondObj);
-        value++;
-      }
-
-      return this.shuffle(cards);
-    },
-    shuffle: function (cards) {
-      let currentIndex = cards.length, temporaryValue, randomIndex;
-
-      // While there remain elements to shuffle...
-      while (0 !== currentIndex) {
-
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        // And swap it with the current element.
-        temporaryValue = cards[currentIndex];
-        cards[currentIndex] = cards[randomIndex];
-        cards[randomIndex] = temporaryValue;
-      }
-      return cards;
-
-    },
-    selected: function (hexagon) {
-
-      if(!this.first) {
-        this.$store.dispatch('selectHexagon',{hexagon: hexagon,position:1});
-        hexagon.showValue = true;
-        this.tempCount++;
-
-        if(this.tempCount % 3 === 0 || this.tempCount % 2 === 0) {
-          this.hexagons.forEach(function(hexaItem) {
-            if(hexaItem.id !== hexagon.id) {
-              hexaItem.showValue = false;
-            }
-          });
-          this.tempCount = 0;
+    data: function () {
+        return {
+            hexagons: [],
+            colors: ['h-color2', 'h-color1', 'h-color3'],
+            tempCount: 0,
         }
-
-        return;
-      }
-
-      if(!this.second) {
-
-        if(hexagon === this.first) { return; }
-
-        this.$store.dispatch('selectHexagon',{hexagon: hexagon,position:2});
-        hexagon.showValue = true;
-        this.tempCount++;
-      }
-
-      if(this.first && this.second) {
-        this.checkScore();
-      }
     },
-    checkScore: function () {
-      if(this.first.value === this.second.value) {
-        this.$store.dispatch('updateScore',{value: this.first.value});
-
-        this.hexagons.splice(this.hexagons.indexOf(this.first), 1);
-        this.hexagons.splice(this.hexagons.indexOf(this.second), 1);
-        this.tempCount = 0;
-      } else {
-        this.$store.dispatch('updateScore',{value: -1});
-      }
-
-      this.resetHexagons();
+    computed: {
+        score() {
+            return this.$store.state.score;
+        },
+        first() {
+            return this.$store.state.first_piece;
+        },
+        second() {
+            return this.$store.state.second_piece;
+        },
     },
-    resetHexagons: function () {
-      this.$store.dispatch('selectHexagon',{hexagon: null,position:1});
-      this.$store.dispatch('selectHexagon',{hexagon: null,position:2});
-    },
-    restartGame: function () {
-      this.hexagons = this.generateHexagons(20);
-      this.resetHexagons();
-      this.$store.dispatch('resetScore');
+    methods: {
+        generateHexagons: function (objectCount) {
+            let cards = [];
+            let value = 1;
+            for (let i = 0; i < objectCount; i += 2) {
+
+                let firstObj = {
+                    id: i,
+                    value: value,
+                    showValue: false,
+                    color: this.colors[Math.floor(Math.random() * this.colors.length)],
+                };
+                let secondObj = {
+                    id: i + 1,
+                    value: value,
+                    showValue: false,
+                    color: this.colors[Math.floor(Math.random() * this.colors.length)],
+                };
+                cards.push(firstObj);
+                cards.push(secondObj);
+                value++;
+            }
+
+            return this.shuffle(cards);
+        },
+        shuffle: function (cards) {
+            let currentIndex = cards.length, temporaryValue, randomIndex;
+
+            // While there remain elements to shuffle...
+            while (0 !== currentIndex) {
+
+                // Pick a remaining element...
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
+
+                // And swap it with the current element.
+                temporaryValue = cards[currentIndex];
+                cards[currentIndex] = cards[randomIndex];
+                cards[randomIndex] = temporaryValue;
+            }
+            return cards;
+
+        },
+        selected: function (hexagon) {
+
+            if (!this.first) {
+                this.$store.dispatch('selectHexagon', {hexagon: hexagon, position: 1});
+                hexagon.showValue = true;
+                this.tempCount++;
+
+                if (this.tempCount % 3 === 0 || this.tempCount % 2 === 0) {
+                    this.hexagons.forEach(function (hexaItem) {
+                        if (hexaItem.id !== hexagon.id) {
+                            hexaItem.showValue = false;
+                        }
+                    });
+                    this.tempCount = 0;
+                }
+
+                return;
+            }
+
+            if (!this.second) {
+
+                if (hexagon === this.first) {
+                    return;
+                }
+
+                this.$store.dispatch('selectHexagon', {hexagon: hexagon, position: 2});
+                hexagon.showValue = true;
+                this.tempCount++;
+            }
+
+            if (this.first && this.second) {
+                this.checkScore();
+            }
+        },
+        checkScore: function () {
+            if (this.first.value === this.second.value) {
+                this.$store.dispatch('updateScore', {value: this.first.value});
+
+                this.hexagons.splice(this.hexagons.indexOf(this.first), 1);
+                this.hexagons.splice(this.hexagons.indexOf(this.second), 1);
+                this.tempCount = 0;
+            } else {
+                this.$store.dispatch('updateScore', {value: -1});
+            }
+
+            this.resetHexagons();
+        },
+        resetHexagons: function () {
+            this.$store.dispatch('selectHexagon', {hexagon: null, position: 1});
+            this.$store.dispatch('selectHexagon', {hexagon: null, position: 2});
+        },
+        restartGame: function () {
+            this.hexagons = this.generateHexagons(20);
+            this.resetHexagons();
+            this.$store.dispatch('resetScore');
+        }
     }
-  }
 }
 </script>
 
 <style>
 
-  @import './assets/styles/main.css';
+@import './assets/styles/main.css';
 
-  #app {
+#app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
     height: 100%;
 }
-  body {
+
+body {
     background: url(./assets/background.jpg) no-repeat center center fixed;
     -webkit-background-size: cover;
     -moz-background-size: cover;
     -o-background-size: cover;
     background-size: cover;
-  }
+}
 
-  .hexagons-wrapper {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(100px, 1fr) [col-start]);
-    row-gap: 10px;
-    column-gap:80px;
-    padding: 15px;
-    justify-items: center;
-  }
-
-  .score {
+.score {
     background-color: whitesmoke;
     color: #0f0f0f;
     padding: 15px;
@@ -177,16 +176,17 @@ export default {
     border-top: 1px solid #8C6723;
     box-shadow: inset 0 0 2px #8C6723;
     font-weight: bold;
-  }
+}
 
-  .show-hexagons-enter-active, .show-hexagons-leave-active {
+.show-hexagons-enter-active, .show-hexagons-leave-active {
     transition: opacity .5s;
-  }
-  .show-hexagons-enter, .show-hexagons-leave-to {
-    opacity: 0;
-  }
+}
 
-  .show-hexagons-move {
+.show-hexagons-enter, .show-hexagons-leave-to {
+    opacity: 0;
+}
+
+.show-hexagons-move {
     transition: opacity 1s;
-  }
+}
 </style>
--- src/components/Hexagon.vue
+++ src/components/Hexagon.vue
@@ -1,109 +1,32 @@
 <template>
-  <div class="hexagon-wrapper">
     <div class="hexagon" :class="parentData.color" @click="select(parentData)">
-      <transition name="fade">
-        <span v-if="parentData.showValue" class="value-span">{{parentData.value}}</span>
-      </transition>
+        <transition name="fade">
+            <span v-if="parentData.showValue" class="value-span">{{parentData.value}}</span>
+        </transition>
     </div>
-  </div>
-
 </template>
 
 <script>
 export default {
-  name: 'Hexagon',
-  props: {
-    parentData: Object,
-  },
-  methods: {
-    select: function (hexagon) {
-      this.$emit('selected',hexagon);
-    }
-  },
+    name: 'Hexagon',
+    props: {
+        parentData: Object,
+    },
+    methods: {
+        select: function (hexagon) {
+            this.$emit('selected', hexagon);
+        }
+    },
 }
 </script>
 
 <style scoped>
-  .hexagon-wrapper {
-    padding-right: 5px;
-    padding-bottom: 30px;
-    padding-top: 30px;
-    position: relative;
-  }
 
-  .hexagon {
-    margin-top: 30px;
-    width: 104px;
-    height: 60px;
-    position: relative;
-  }
-
-  .hexagon::before {
-    content: " ";
-    width: 0; height: 0;
-    border-left: 52px solid transparent;
-    border-right: 52px solid transparent;
-    position: absolute;
-    top: -30px;
-  }
-
-  .hexagon::after {
-    content: "";
-    width: 0;
-    position: absolute;
-    bottom: -30px;
-    border-left: 52px solid transparent;
-    border-right: 52px solid transparent;
-  }
-
-  .hexagon:hover {
-    cursor: pointer;
-  }
-
-  .h-color1 {
-    background-color: #D9A036;
-    transition: box-shadow 0.3s ease-in-out;
-  }
-
-  .h-color1:hover,.h-color2:hover,.h-color3:hover {
-    box-shadow:
-            0 0 20px #F2E750,            /* outer  */
-            -10px 0 50px #D9A036,        /* outer left  */
-            10px 0 50px #8C6723;         /* outer right  */
-  }
-
-  .h-color1::before { border-bottom: 30px solid #D9A036; }
-  .h-color1::after { border-top: 30px solid #D9A036; }
-
-  .h-color2 {
-    background-color: #8C6723;
-    transition: box-shadow 0.3s ease-in-out;
-  }
-
-  .h-color2::before { border-bottom: 30px solid #8C6723; }
-  .h-color2::after { border-top: 30px solid #8C6723; }
-
-  .h-color3 {
-    background-color: #403014;
-    transition: box-shadow 0.3s ease-in-out;
-  }
-
-  .h-color3::before { border-bottom: 30px solid #403014; }
-  .h-color3::after { border-top: 30px solid #403014; }
-
-  .value-span {
-    position: absolute;
-    font-weight: bold;
-    color: black;
-    font-size: 2em;
-    left: 40px;
-    padding-top: 12px;
-  }
-
-  .fade-enter-active, .fade-leave-active {
+.fade-enter-active, .fade-leave-active {
     transition: opacity .5s;
-  }
-  .fade-enter, .fade-leave-to {
+}
+
+.fade-enter, .fade-leave-to {
     opacity: 0;
-  }
+}
 </style>
